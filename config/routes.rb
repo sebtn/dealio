daa5e3eba9a0b1aef930ab4164ce4419af774904
sebtn <tonomudo@gmail.com>
2015-11-24 22:55:54 +0000

    Create index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Dealio::Application.routes.draw do
+  root 'deals#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
