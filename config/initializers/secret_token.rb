6f1763f698ba68512e0077b76b957c56c4e4e850
sebtn <tonomudo@gmail.com>
2015-11-24 22:13:41 +0000

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dealio::Application.config.secret_key_base = '2f11b3cd777805412c7a960c6da0947545aaacfab944c7326c0f8a7bb3241d176e8db4217c687d42053a57ec9fa564163ca629540b45a913dc9fa334e0be127b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dealio::Application.config.session_store :cookie_store, key: '_dealio_session'
